2d80ffae707423c9ee931e990c9da8bcb05408ea
Yoro2002 <user.dipeoluayobami2@gmail.com>
2024-05-07 15:02:51 +0100

    Added Cart Section to Icon

--- main.js
+++ main.js
@@ -5,4 +5,14 @@ let navbar = document.querySelector(".navbar");
 menu.onclick = () => {
     navbar.classList.toggle("active");
     menu.classList.toggle("move");
+    cart.classList.remove("active");
+}
+
+// Cart Toggle 
+let cart = document.querySelector(".cart");
+
+document.querySelector("#cart-icon").onclick = () => {
+    cart.classList.toggle("active");
+    navbar.classList.remove("active");
+    menu.classList.remove("move");
 }
