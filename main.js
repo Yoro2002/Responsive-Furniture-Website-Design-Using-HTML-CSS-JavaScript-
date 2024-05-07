3d868f2ee17018ff67d3cbce2b4218977db4860c
Yoro2002 <user.dipeoluayobami2@gmail.com>
2024-05-07 16:18:30 +0100

    Added Login Form

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ menu.onclick = () => {
     navbar.classList.toggle("active");
     menu.classList.toggle("move");
     cart.classList.remove("active");
+    loginForm.classList.remove("active");
 }
 
 // Cart Toggle 
@@ -13,6 +14,17 @@ let cart = document.querySelector(".cart");
 
 document.querySelector("#cart-icon").onclick = () => {
     cart.classList.toggle("active");
+    navbar.classList.remove("active");
+    menu.classList.remove("move");
+    loginForm.classList.remove("active");
+}
+
+// Login Form Toggle
+let loginForm = document.querySelector(".login-form");
+
+document.querySelector("#user-icon").onclick = () => {
+    loginForm.classList.toggle("active");
+    cart.classList.remove("active");
     navbar.classList.remove("active");
     menu.classList.remove("move");
 }
